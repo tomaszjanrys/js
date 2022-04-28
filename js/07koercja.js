759ffad4ce10ede5a166fadd82bca7eee3936118
tomek <ok>
2022-04-28 22:12:59 +0200

    koercja jawna i niejawna

---
+++ js/07koercja.js
@@ -0,0 +1,27 @@
+if(true) console.log("ok");
+if("10" == String(10)) console.log("konwersja jawna");
+if("10" - 5 ==5) console.log("konwersja  nie jawna");
+if(Number("10") - 5 ==5) console.log("1 konwersja  Number (\"\\10\") - 5 ==5)  jawna");
+if(parseInt("10s2dads") - 5 ==5) console.log("1 konwersja parseInt `10dsadd`  - 5 ==5)  jawna");
+console.log(Boolean())
+console.log(Boolean(""))
+console.log(Boolean(NaN))
+console.log(Boolean(null))
+console.log(Boolean(void 0))
+console.log(Boolean(-0))
+console.log(Boolean(0))
+console.log(Boolean(Infinity))
+console.log(Boolean([]))
+console.log(Boolean({}))
+console.log(Boolean(Number(!!!!!!!!!!!!0)))
+console.log(Boolean(Boolean()))
+
+let tab = new Array()
+tab = [1,2,3,4].join("-")
+console.log(tab+ "typ",typeof tab)
+
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -9,5 +9,6 @@
 > | 04 | literals , slice, split, reg, indexof, replace |
 > | 05 | Arrays push pop lenght, delete shift, foreach, for, reverse, indexOf,join, concat, fill|
 > | 06 |array splice |
+> | 06 | explicit and implicit coercion |
 
 
